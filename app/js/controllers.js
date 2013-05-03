c4295b79fe70b6967c9933893d5de42729304f04
Leo Shmuylovich <leoshmu@gmail.com>
2013-05-03 15:53:49 -0400

    first commit

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,23 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+function MyController($scope, ThingOne, ThingTwo){
+  $scope.dataOne = ThingOne.get();
 
-  }])
-  .controller('MyCtrl2', [function() {
+    // get data with passed in parameter
+  $scope.dataTwo= ThingTwo.get({fileName: "data_sample"});
+}
 
-  }]);
+/* The new seed has the structure below for the controllers but I can't figure out how to make it work yet
+so I am sticcking to the code above, which is the old way!!
+*/
+// angular.module('myApp.controllers', []).
+//   controller('MyController', [function($scope) {
+
+//     // call service with hardcoded filename
+//     $scope.dataOne = ThingOne.get();
+
+//     // get data with passed in parameter
+//     $scope.dataTwo= ThingTwo.get({fileName: "data_sample"});
+
+//   }]);
--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,16 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+angular.module('myApp.services', ['ngResource']).
+  // first factory has hardcoded data
+  factory('ThingOne', function($resource){
+    return $resource('data/data_sample.json', {},{
+      get: {method: 'GET', isArray:true}
+   });
+  }).
+  // second factory hasa  fileName parameter we can pass in
+  factory('ThingTwo', function($resource){
+    return $resource('data/:fileName.json', {},{
+      get: {method: 'GET', isArray:true}
+    });
+  });
